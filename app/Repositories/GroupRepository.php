429d5d007e0a25c00f7cf73e490b0e98bffef78f
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-03-30 23:00:46 +0200

    init coding challange

---
+++ app/Repositories/GroupRepository.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Repositories\Api\GroupRepositoryInterface;
+
+/**
+ * Group repository layer
+ */
+class GroupRepository implements GroupRepositoryInterface
+{
+
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Repositories\Api\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+
+}
