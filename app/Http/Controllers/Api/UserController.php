d4da93ac8f1c3b08070181e0b48dc67d952f557d
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-01 16:53:03 +0200

    implement userController class

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,21 +1,133 @@
 <?php
 declare(strict_types=1);
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Services\Api\UserServiceInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
+/**
+ * User Controller class
+ */
 class UserController extends Controller
 {
+    /**
+     * @param UserServiceInterface $userService
+     */
     public function __construct(
         protected UserServiceInterface $userService
-    )
-    {
-    }
+    ){}
 
+    /**
+     * Get All users
+     *
+     * @return JsonResponse
+     */
     public function index()
     {
-        dd($this->userService->getUsers());
+        return response()->json(['users' => UserResource::collection($this->userService->getUsers())]);
+    }
+
+    /**
+     * Get User By Id
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id)
+    {
+        try {
+            $user = $this->userService->getUser($id);
+
+            return response()->json(['data' => new UserResource($user)]);
+        } catch (ModelNotFoundException|\Exception $ex) {
+            return response()->json(['data' => null], 404);
+        }
+    }
+
+    /**
+     * Store New user
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $data = $this->getRequestedData($request);
+
+        try {
+            $user = $this->userService->addUser($data);
+
+            return response()->json(['data' => new UserResource($user)], 201);
+        } catch (ValidationException $ex) {
+            return response()->json(['errors' => $ex->errors()], 403);
+        } catch (\Exception $ex) {
+            return response()->json(['message' => __('Internal Server Error')], 500);
+        }
+    }
+
+    /**
+     * Update Existing User
+     *
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function update(int $id, Request $request)
+    {
+        $data = $this->getRequestedData($request);
+        try {
+            $user = $this->userService->editUser($id, $data);
+
+            return response()->json(['data' => new UserResource($user)]);
+        } catch (ValidationException $ex) {
+            return response()->json(['errors' => $ex->errors()], 403);
+        } catch (\Exception $ex) {
+            return response()->json(['message' => __('Internal Server Error')], 500);
+        }
+    }
+
+    /**
+     * Delete existing user
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function delete(int $id)
+    {
+        try {
+            $deleted = $this->userService->deleteUser($id);
+
+            return response()->json(['deleted' => $deleted], 200);
+        } catch (ModelNotFoundException $ex) {
+            return response()->json(['message' => __('Sorry!, this user is not exists')], 404);
+        } catch (\Exception|\Throwable $ex) {
+            return response()->json(['message' => __('Internal Server Error')], 500);
+        }
+    }
+
+    /**
+     * Get the posted Data from request
+     *
+     * @param Request $request
+     * @return array
+     */
+    protected function getRequestedData(Request $request): array
+    {
+        return $request->only([
+            'username',
+            'firstname',
+            'lastname',
+            'phone',
+            'age',
+            'type',
+            'email',
+            'password'
+        ]);
     }
 }
