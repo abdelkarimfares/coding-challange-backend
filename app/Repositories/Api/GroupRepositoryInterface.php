6ee02bcddbe084e0a1aee58bc8d8b29208cd5a6b
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-01 17:51:23 +0200

    implement group repository

--- app/Repositories/Api/GroupRepositoryInterface.php
+++ app/Repositories/Api/GroupRepositoryInterface.php
@@ -1,8 +1,30 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Repositories\Api;
 
+use App\Models\Group;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+/**
+ * Interface GroupRepositoryInterface
+ */
 interface GroupRepositoryInterface
 {
+    /**
+     * Get All groups
+     *
+     * @return Collection
+     */
+    public function getAllGroups(): Collection;
 
+    /**
+     * Get group by id
+     *
+     * @param int $id
+     * @return Group
+     * @throws ModelNotFoundException
+     */
+    public function getById(int $id): Group;
 }
--- app/Repositories/GroupRepository.php
+++ app/Repositories/GroupRepository.php
@@ -3,12 +3,35 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Models\Group;
 use App\Repositories\Api\GroupRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * Group repository layer
  */
 class GroupRepository implements GroupRepositoryInterface
 {
+    /**
+     * @param Group $group
+     */
+    public function __construct(
+        protected Group $group
+    ){}
 
+    /**
+     * @inheritDoc
+     */
+    public function getAllGroups(): Collection
+    {
+        return $this->group->get();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getById(int $id): Group
+    {
+        return $this->group->findOrFail($id);
+    }
 }
