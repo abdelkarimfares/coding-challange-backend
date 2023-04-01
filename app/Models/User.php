16d3d53720a40b5e424f09b81fbffb65f25898ae
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-01 16:09:35 +0200

    add user fillable fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'firstname',
+        'lastname',
+        'phone',
+        'age',
+        'type',
         'email',
-        'password',
+        'password'
     ];
 
     /**
