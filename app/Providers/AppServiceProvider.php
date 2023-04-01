9f41d559d7a490c8c10bc7e04774e6cad7af455f
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-01 17:54:16 +0200

    binding group classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Repositories\Api\GroupRepositoryInterface;
 use App\Repositories\Api\UserRepositoryInterface;
+use App\Repositories\GroupRepository;
 use App\Repositories\UserRepository;
+use App\Services\Api\GroupServiceInterface;
 use App\Services\Api\UserServiceInterface;
+use App\Services\GroupService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserServiceInterface::class, UserService::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
+        $this->app->bind(GroupServiceInterface::class, GroupService::class);
+        $this->app->bind(GroupRepositoryInterface::class, GroupRepository::class);
     }
 
     /**
