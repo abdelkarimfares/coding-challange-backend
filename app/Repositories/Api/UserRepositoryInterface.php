6af543db90d8a3e6481fbbbca3ace727e60fe9be
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-01 16:06:02 +0200

    Implement User Repository methods

--- app/Repositories/Api/UserRepositoryInterface.php
+++ app/Repositories/Api/UserRepositoryInterface.php
@@ -1,8 +1,48 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Repositories\Api;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+/**
+ * Interface UserRepositoryInterface
+ */
 interface UserRepositoryInterface
 {
+    /**
+     * Get All users
+     *
+     * @return Collection
+     */
+    public function getAllUsers(): Collection;
 
+    /**
+     * Get user by id
+     *
+     * @param int $id
+     * @return User
+     * @throws Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function getById(int $id): User;
+
+    /**
+     * Create new user
+     *
+     * @param array $data
+     * @return User
+     */
+    public function createUser(array $data): User;
+
+    /**
+     * Update User Data
+     *
+     * @param int $id
+     * @param array $data
+     * @return User
+     * @throws ModelNotFoundException
+     */
+    public function updateUser(int $id, array $data): User;
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,9 +3,59 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Models\User;
 use App\Repositories\Api\UserRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * Class UserRepository
+ */
 class UserRepository implements UserRepositoryInterface
 {
+    /**
+     * @param User $user
+     */
+    public function __construct(
+        protected User $user
+    ){}
 
+    /**
+     * @inheritDoc
+     */
+    public function getAllUsers(): Collection
+    {
+        return $this->user->get();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getById(int $id): User
+    {
+        return $this->user->findOrFail($id);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createUser(array $data): User
+    {
+        $user = new User($data);
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function updateUser(int $id, array $data): User
+    {
+        $user = $this->getById($id);
+        foreach ($data as $key => $value) {
+            $user->setAttribute($key, $value);
+        }
+        $user->save();
+
+        return $user;
+    }
 }
