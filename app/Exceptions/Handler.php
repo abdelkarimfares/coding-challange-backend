bd1d9a3c587e0535ca3aa6929b7efde901f20fe5
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-02 15:13:26 +0200

    Handling Unauthorized Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,5 +45,19 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * @param $request
+     * @param AuthenticationException $ex
+     * @return \Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->is('api/*')) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return parent::unauthenticated($request, $exception);
     }
 }
