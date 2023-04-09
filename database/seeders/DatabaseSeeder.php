2b2b6717d448254a31019ac4065eede8941cecfb
abdelkarimfares <abdelkarimfares94@gmail.com>
2023-04-09 18:20:08 +0200

    fake users & groups generation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Group;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $groups = Group::factory()->count(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory(50)
+             ->hasAttached($groups)
+             ->create();
+
+         \App\Models\User::factory()->count(1)->create([
+             'username' => 'admin',
+             'email' => 'admin@gmail.com',
+             'firstname' => fake()->firstName(),
+             'lastname' => fake()->lastName(),
+             'phone' => fake()->phoneNumber(),
+             'age' => fake()->numberBetween(18, 100),
+             'type' => fake()->randomElement(['admin']),
+             'email_verified_at' => now(),
+             'password' => Hash::make('admin'),
+             'remember_token' => Str::random(10),
+         ]);
     }
 }
